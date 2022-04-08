7f216c4d66ac8828be4f4faf897c5c4767e5094b
tordtaflowo <103199170+tordtaflowo@users.noreply.github.com>
2022-04-08 18:49:35 -0400

    Add files via upload

---
+++ cachipun.py
@@ -0,0 +1,34 @@
+J1 = float(input("Jugador 1, ingrese una opcion: \n 1) Piedra \n 2) Papel \n 3) Tijeras \n "))
+
+if J1 > 3 :
+    print("Eliga una opcion valida")
+    
+else :
+    J2 = float(input("Jugador 2, ingrese una opcion: \n 1) Piedra \n 2) Papel \n 3) Tijeras \n "))
+
+    if J2 > 3 :
+        print("Eliga una opcion valida")
+
+
+if J1 == 1:
+    if J2 == 1:
+        print("Es un empate")
+    elif J2 == 2:
+        print("El jugador 2 gana")
+    elif J2 == 3:
+        print("El jugador 1 gana")
+elif J1 == 2:
+    if J2 == 1:
+        print("El jugador 1 gana")
+    elif J2 == 2:
+        print("Es un empate")
+    elif J2 == 3:
+        print("El jugador 2 gana")
+elif J1 == 3:
+    if J2 == 1:
+        print("El jugador 2 gana")
+    elif J2 == 2:
+        print("El jugador 1 gana")
+    elif J2 == 3:
+         print("Es un empate")
+
